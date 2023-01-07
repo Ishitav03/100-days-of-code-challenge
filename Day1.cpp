f409815f736bfeac0eb95150c8a2c74ceb269243
Ishita Verma <80648745+Ishitav03@users.noreply.github.com>
2023-01-07 23:04:29 +0530

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -1,16 +1,21 @@
-//984. String Without AAA or BBB
-//Given two integers a and b, return any string s such that: s has length a + b and contains exactly a 'a' letters, and exactly b 'b' letters,
-//The substring 'aaa' does not occur in s, and
-//The substring 'bbb' does not occur in s.
+//290. Word Pattern
+//Given a pattern and a string s, find if s follows the same pattern.
+//Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
 
 
 class Solution {
 public:
-    string strWithout3a3b(int a, int b) {        
-        if(a == 0) return string(b, 'b');
-        else if(b == 0) return string(a, 'a');
-        else if(a == b) return "ab" + strWithout3a3b(a - 1, b- 1);
-        else if(a > b) return "aab" + strWithout3a3b(a - 2, b- 1);
-        else return strWithout3a3b(a-1,b-2) + "abb";
+    bool wordPattern(string pattern, string s) {
+        map<char, int> p;
+        map<string, int> w;
+        istringstream in(s);
+        
+        int i = 0, n = pattern.size();
+        for (string word; in >> word; ++i) {
+            if (i == n || p[pattern[i]] != w[word])
+                return false;
+            p[pattern[i]] = w[word] = i + 1;
+        }
+        return i == n;
     }
-}; 
+};
