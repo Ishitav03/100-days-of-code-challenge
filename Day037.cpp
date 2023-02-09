cc6d07974b22ece493c9f8b504be35d19ce45de8
Ishita Verma <80648745+Ishitav03@users.noreply.github.com>
2023-02-10 01:27:13 +0530

    Update Day037.cpp

--- Day037.cpp
+++ Day037.cpp
@@ -1,1 +1,27 @@
+//1470. Shuffle the Array
+//Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 
+//Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+
+//Constraints:
+
+// 1 <= n <= 500
+// nums.length == 2n
+// 1 <= nums[i] <= 10^3
+
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {        
+        for(int i = n; i<nums.size(); i++) {
+            nums[i] = (nums[i] << 10) | nums[i - n];
+        }
+        int ind = 0;
+        for(int i = n; i <nums.size(); i++, ind += 2) {
+            nums[ind] = nums[i] & 1023;
+            nums[ind + 1] = nums[i] >> 10;
+        }
+        return nums;
+    }
+};
