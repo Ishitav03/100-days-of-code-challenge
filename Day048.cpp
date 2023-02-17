5ff6139ab8ee62a436442f7661d0d8fa7bbb529f
Ishita Verma <80648745+Ishitav03@users.noreply.github.com>
2023-02-17 19:58:17 +0530

    Create Day048.cpp

---
+++ Day048.cpp
@@ -0,0 +1,9 @@
+//783. Minimum Distance Between BST Nodes
+//Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different nodes in the tree.
+
+//Constraints:
+
+// The number of nodes in the tree is in the range [2, 100].
+// 0 <= Node.val <= 10^5
+
+
