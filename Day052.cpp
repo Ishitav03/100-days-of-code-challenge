6be02d244c7f7da7521d109cdb3255cdc4938643
Ishita Verma <80648745+Ishitav03@users.noreply.github.com>
2023-02-22 01:54:13 +0530

    Update Day052.cpp

--- Day052.cpp
+++ Day052.cpp
@@ -10,3 +10,14 @@
 // 0 <= nums[i] <= 10^5
 
 
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int res=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            res^=nums[i];
+        }
+        return res;
+    }
+};
