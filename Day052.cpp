7c845163bc6a78455a5f0896bfb978f3fffb1609
Ishita Verma <80648745+Ishitav03@users.noreply.github.com>
2023-02-21 15:32:07 +0530

    Create Day052.cpp

---
+++ Day052.cpp
@@ -0,0 +1,12 @@
+//540. Single Element in a Sorted Array
+//You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+// Return the single element that appears only once.
+// Your solution must run in O(log n) time and O(1) space.
+  
+  
+//  Constraints:
+// 1 <= nums.length <= 10^5
+// 0 <= nums[i] <= 10^5
+
+
