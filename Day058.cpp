afd4ff7ba3f13ef94ae8f34fee57901463943a7f
Ishita Verma <80648745+Ishitav03@users.noreply.github.com>
2023-02-27 14:07:57 +0530

    Create Day058.cpp

---
+++ Day058.cpp
@@ -0,0 +1,21 @@
+// 427. Construct Quad Tree
+
+// Given a n * n matrix grid of 0's and 1's only. We want to represent the grid with a Quad-Tree.
+
+// Return the root of the Quad-Tree representing the grid.
+
+// Notice that you can assign the value of a node to True or False when isLeaf is False, and both are accepted in the answer.
+
+// A Quad-Tree is a tree data structure in which each internal node has exactly four children. Besides, each node has two attributes:
+
+// val: True if the node represents a grid of 1's or False if the node represents a grid of 0's.
+// isLeaf: True if the node is leaf node on the tree or False if the node has the four children.
+  
+   
+
+// Constraints:
+
+// n == grid.length == grid[i].length
+// n == 2x where 0 <= x <= 6
+
+
