2f8fe31224b853cd2e9161feb678f516c500789b
Ishita Verma <80648745+Ishitav03@users.noreply.github.com>
2023-03-22 01:03:28 +0530

    Create Day080.cpp

---
+++ Day080.cpp
@@ -0,0 +1,10 @@
+// 2348. Number of Zero-Filled Subarrays
+// Given an integer array nums, return the number of subarrays filled with 0.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+  
+//   Constraints:
+
+// 1 <= nums.length <= 10^5
+// -109 <= nums[i] <= 10^9
+
