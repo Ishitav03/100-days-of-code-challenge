7e3ca103f5a36a306e097a36eb3864d0f564410e
Ishita Verma <80648745+Ishitav03@users.noreply.github.com>
2023-03-22 01:33:14 +0530

    Update Day080.cpp

--- Day080.cpp
+++ Day080.cpp
@@ -8,3 +8,22 @@
 // 1 <= nums.length <= 10^5
 // -109 <= nums[i] <= 10^9
 
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        typedef long long ll;
+        ll count =0,
+        res=0;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]==0 ) count++;
+            else if(count != 0){
+                res += count*(count+1)/2;
+                count=0;
+            }
+        } 
+        if(count>0){
+            res += count*(count+1)/2;
+        }
+        return res;
+    }
+};
